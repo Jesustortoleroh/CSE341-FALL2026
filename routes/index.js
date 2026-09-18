99b23a393668ee3d0b6a8ecf09cf9e064cf932e2
Jesustortoleroh <tor19021@byui.edu>
2026-09-18 17:13:09 -0400

    Fix route handler case sensitivity for the root path

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const routes = require('express').Router();
 const lesson1Controller = require('../controllers/lesson1.js');
 
 
-routes.get('/', lesson1Controller.RoseRoute);
+routes.get('/', lesson1Controller.roseRoute);
 routes.get('/peter', lesson1Controller.peterRoute);
 routes.get('/carla', lesson1Controller.carlaRoute);
 
